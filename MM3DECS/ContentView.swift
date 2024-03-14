93c3b5c1450af3933b65a163fa1149c6776699f0
Mohammad Jeragh <mjeragh@gmail.com>
2024-03-14 12:28:32 +0300

    Initial Commit

---
+++ MM3DECS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MM3DECS
+//
+//  Created by Mohammad Jeragh on 14/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MM3DECS/MM3DECSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MM3DECSApp.swift
+//  MM3DECS
+//
+//  Created by Mohammad Jeragh on 14/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct MM3DECSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
