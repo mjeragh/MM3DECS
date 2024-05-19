3c409b9c406f9213c9afed3226e4d7a948c004ae
Mohammad Jeragh <mjeragh@gmail.com>
2024-05-19 18:13:00 +0300

    updated common indicies

--- MM3DECS/Shaders/Common.h
+++ MM3DECS/Shaders/Common.h
@@ -12,73 +12,62 @@
 #import "stdbool.h"
 
 
-typedef struct {
-    float x;
-    float y;
-} twoDPoint;
-
-typedef struct {
-    matrix_float4x4 modelMatrix;
-    matrix_float4x4 viewMatrix;
-    matrix_float4x4 projectionMatrix;
-    matrix_float3x3 normalMatrix;
-    float width;
-    float height;
-    matrix_float4x4 shadowProjectionMatrix;
-    matrix_float4x4 shadowViewMatrix;
-    twoDPoint point;
-//    vector_float4 clipPlane;
-} Uniforms;
-
+typedef enum {
+  VertexBuffer = 0,
+  UVBuffer = 1,
+  TangentBuffer = 2,
+  BitangentBuffer = 3,
+  UniformsBuffer = 11,
+  ParamsBuffer = 12,
+  LightBuffer = 13,
+  MaterialBuffer = 14,
+  JointBuffer = 15,
+  ModelTransformBuffer = 16,
+  InstancesBuffer = 17,
+} BufferIndices;
 
 typedef enum {
-    BaseColor = 0,
-    NormalTexture = 1,
-    RoughnessTexture = 2,
-    MetallicTexture = 3,
-    AOTexture = 4,
-    OpacityTexture = 5,
-    ShadowTexture = 10,
-    SkyboxTexture = 11,
-    SkyboxDiffuseTexture = 12,
-    BRDFLutTexture = 13,
-    PositionTexture = 14,
-    MiscTexture = 31
-} TexturesIndices;
+  Position = 0,
+  Normal = 1,
+  UV = 2,
+  Tangent = 3,
+  Bitangent = 4,
+  Joints = 6,
+  Weights = 7
+} Attributes;
 
-typedef struct{
-    vector_float3 localOrigin;
-    vector_float3 localDirection;
-} LocalRay;
-
-typedef struct {
-  float width;
-  float height;
-  uint tiling;
-  uint lightCount;
-  vector_float3 cameraPosition;
-  bool alphaBlending;
-  bool transparency;
-} Params;
+typedef enum {
+  BaseColor = 0,
+  NormalTexture = 1,
+  RoughnessTexture = 2,
+  MetallicTexture = 3,
+  AOTexture = 4,
+  OpacityTexture = 5,
+  ShadowTexture = 15,
+  SkyboxTexture = 16,
+  SkyboxDiffuseTexture = 17,
+  BRDFLutTexture = 18,
+  MiscTexture = 31
+} TextureIndices;
 
 typedef enum {
   unused = 0,
   Sun = 1,
   Spot = 2,
-  Dot = 3, //Point in the book
+  Point = 3,
   Ambient = 4
 } LightType;
 
 typedef struct {
-    vector_float3 position;
-    vector_float3 color;
-    vector_float3 specularColor;
-    float intensity;
-    vector_float3 attenuation;
-    LightType type;
-    float coneAngle;
-    vector_float3 coneDirection;
-    float coneAttenuation;
+  LightType type;
+  vector_float3 position;
+  vector_float3 color;
+  vector_float3 specularColor;
+  float radius;
+  vector_float3 attenuation;
+  float coneAngle;
+  vector_float3 coneDirection;
+  float coneAttenuation;
 } Light;
 
 typedef enum {
